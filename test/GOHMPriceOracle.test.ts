465bc8387216437b08784bf6b670767693b3aa4e
conlotor <stepcek80@gmail.com>
2023-07-05 01:40:12 +1000

    add GOHMPrice oracle

---
+++ test/GOHMPriceOracle.test.ts
@@ -0,0 +1,27 @@
+import { ethers } from 'hardhat';
+
+import { ChainlinkPriceFactory, GOHMPriceOracle } from '../types';
+import { deployContract } from '../helpers/contract';
+
+const factoryAddr = '0xB08742E82cC6743D8a1Cf2473aD36c9Ea9D477fD';
+const gOHM = '0x0ab87046fbb341d058f17cbc4c1133f25a20a52f';
+const OHM = '0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5';
+
+describe('GOHMPriceOracle', async () => {
+  let GOHMPriceOracle: GOHMPriceOracle;
+
+  let accounts: any[];
+
+  it('GOHM price', async () => {
+    accounts = await ethers.getSigners();
+
+    GOHMPriceOracle = await deployContract<GOHMPriceOracle>('GOHMPriceOracle', [
+      factoryAddr,
+      gOHM,
+      OHM,
+    ]);
+
+    const ethPrice = await GOHMPriceOracle.latestAnswer();
+    console.log('ethPrice: ', ethPrice.toString());
+  });
+});
